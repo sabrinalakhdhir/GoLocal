22acc5cec857b2ce33afde1d4f70dc1b72eaea9a
Jacky Tang <jickay@gmail.com>
2018-11-09 10:07:41 -0700

    Test code for routing and Angular data binding

--- GoLocal/src/pages/home/home.ts
+++ GoLocal/src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AboutPage } from '../about/about'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  private var1 = 1;
+  private arr = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 }
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToAbout() {
+    this.navCtrl.setRoot(AboutPage);
+  }
+
+  increment() {
+    this.var1++;
   }
 
 }
