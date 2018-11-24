04ae99b52975d6dc47c84f79bea2182df0b20327
jickay <jickay@gmail.com>
2018-11-24 12:04:38 -0700

    Update with Kris' files

--- GoLocal/src/pages/home/home.ts
+++ GoLocal/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { NavController, ModalController, NavParams } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { ProfilePage } from '../profile/profile';
@@ -11,13 +13,20 @@ import { LoginPage } from '../login/login';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
 
+  @ViewChild('slides') slides: Slides;
+
   private activities = [
-    { image: 1, title: 'Activity #', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
-    { image: 2, title: 'Activity #', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
-    { image: 3, title: 'Activity #', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
-    { image: 4, title: 'Activity #', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/1.jpg", title: 'Activity ', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/2.jpg", title: 'Activity ', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/3.jpg", title: 'Activity ', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/4.jpg", title: 'Activity ', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/5.jpg", title: 'Activity ', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
+    { image: "assets/imgs/GoLocalLogo.png", title: 'Activity', price: 100, description: 'This is a kind of activity description with all the things that you can do!' },
   ];
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
@@ -40,6 +49,12 @@ export class HomePage {
   goToProfile() {
     this.navCtrl.push(ProfilePage);
   }
+  
+  next() {
+    this.slides.slideNext();
+  }
 
-
+  prev() {
+    this.slides.slidePrev();
+  }
 }
